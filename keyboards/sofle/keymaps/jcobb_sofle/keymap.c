8b63166f8027bc5aff289723b0fc55071b683bd4
Jason M Cobb <Jason_M_Cobb@carmax.com>
2021-08-05 17:31:29 -0400

    Forgot to add fold key

--- keyboards/sofle/keymaps/jcobb_sofle/keymap.c
+++ keyboards/sofle/keymaps/jcobb_sofle/keymap.c
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
     KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                         KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, \
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                          KC_LSTRT, KC_PRVWD, KC_UP, KC_NXTWD, KC_LEND, KC_F12, \
-    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                        KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, \
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_FOLD, KC_NO,                        KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, \
     KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,        KC_TRNS, KC_DLINE, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, \
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
     ),
