43f80cbbed6334dcc9fb76ac31ebce4610b2bf4b
Jason Cobb <jasoncobb@MacBook-Pro.jasoncobb.me>
2021-08-25 23:16:03 -0400

    current layouts

--- keyboards/sofle/keymaps/jcobb_sofle/keymap.c
+++ keyboards/sofle/keymaps/jcobb_sofle/keymap.c
@@ -69,13 +69,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_COLEMAK] = LAYOUT( \
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV, \
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSLS, \
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC, \
   KC_LCTL,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
                  KC_LGUI,KC_LALT,KC_LCTRL,KC_LOWER, KC_ENT,      KC_SPC,  KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI \
-),
-
+    ),
 /*
  * GAMING
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -114,11 +113,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_LOWER] = LAYOUT( \
-    KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                         KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, \
-    KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_LPRN, KC_RPRN,                     KC_NO, KC_NO, LCTL(KC_UP), KC_NO, KC_NO, KC_F12, \
-    KC_TRNS, KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,                    KC_NO, LCTL(KC_LEFT), LCTL(KC_DOWN), LCTL(KC_RIGHT), KC_NO, KC_NO, \
-    KC_TRNS, KC_EQL, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_TRNS,        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, \
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
+        KC_ESC,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,                           KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,
+    KC_TRNS,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_LPRN,                       KC_RPRN,KC_1,KC_2,KC_3,KC_NO,KC_F12,
+    KC_TRNS,KC_PERC,KC_AMPR,KC_MINS,KC_EQL,KC_LCBR,                     KC_RCBR,KC_4,KC_5,KC_6,KC_0,KC_DEL,
+    KC_TRNS,KC_CIRC,KC_ASTR,KC_UNDS,KC_PLUS,KC_LBRC,KC_TRNS,      KC_TRNS,KC_RBRC,KC_7,KC_8,KC_9,KC_NO,KC_TRNS,
+            KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,                      KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS
     ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
@@ -136,9 +135,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT( \
     KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                         KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, \
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_JBTERM,                          KC_LSTRT, KC_PRVWD, KC_UP, KC_NXTWD, KC_LEND, KC_F12, \
-    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_JBFOLD, KC_NO,                        KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, \
-    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,        KC_TRNS, KC_DLINE, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, \
+    KC_NO, KC_NO, LCTL(LSFT(KC_TAB)), LCTL(KC_UP), LCTL(KC_TAB), KC_NO,                          KC_NO, KC_PRVWD, KC_UP, KC_NXTWD, KC_NO, KC_F12, \
+    KC_TRNS, KC_NO, LCTL(KC_LEFT), LCTL(KC_DOWN), LCTL(KC_RIGHT), KC_NO,                        KC_LSTRT, KC_LEFT, KC_DOWN, KC_RGHT, KC_LEND, KC_NO, \
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_JBFOLD, KC_JBTERM, KC_TRNS,        KC_TRNS, KC_DLINE, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, \
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
     ),
 /* ADJUST
