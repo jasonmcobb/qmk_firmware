9f4f61107d2d700a8e1a6e7c420a6e122e0cad59
Jason Cobb <jason.cobb@jasoncobb.me>
2021-12-09 19:35:55 -0500

    added my updates

--- keyboards/sofle/keymaps/jcobb_sofle/config.h
+++ keyboards/sofle/keymaps/jcobb_sofle/config.h
@@ -2,6 +2,7 @@
 
 #define OLED_TIMEOUT 30000
 #define OLED_BRIGHTNESS 80
+#define SPLIT_OLED_ENABLE
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
