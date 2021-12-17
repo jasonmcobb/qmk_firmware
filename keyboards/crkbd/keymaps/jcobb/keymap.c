8e34f88ec818e5aaaa948a298b4beecc75c56b04
Jason M Cobb <Jason_M_Cobb@carmax.com>
2021-12-17 16:55:53 -0500

    fixing errorswq

--- keyboards/crkbd/keymaps/jcobb/keymap.c
+++ keyboards/crkbd/keymaps/jcobb/keymap.c
@@ -305,38 +305,6 @@ static void render_luna(int LUNA_X, int LUNA_Y) {
 }
 
 /* KEYBOARD PET END */
-// oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-//   if (!is_keyboard_master()) {
-//     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-//   }
-//   return rotation;
-// }
-
-// #define L_BASE 0
-// #define L_LOWER 2
-// #define L_RAISE 4
-// #define L_ADJUST 8
-
-// void oled_render_layer_state(void) {
-//     oled_write_P(PSTR("Layer: "), false);
-//     switch (layer_state) {
-//         case L_BASE:
-//             oled_write_ln_P(PSTR("Default"), false);
-//             break;
-//         case L_LOWER:
-//             oled_write_ln_P(PSTR("Lower"), false);
-//             break;
-//         case L_RAISE:
-//             oled_write_ln_P(PSTR("Raise"), false);
-//             break;
-//         case L_ADJUST:
-//         case L_ADJUST|L_LOWER:
-//         case L_ADJUST|L_RAISE:
-//         case L_ADJUST|L_LOWER|L_RAISE:
-//             oled_write_ln_P(PSTR("Adjust"), false);
-//             break;
-//     }
-// }
 
 static void print_status_narrow(void) {
     // Print current mode
@@ -397,53 +365,24 @@ static void print_status_narrow(void) {
     /* KEYBOARD PET RENDER END */
 }
 
-
-// char keylog_str[24] = {};
-
-// const char code_to_name[60] = {
-//     ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
-//     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-//     'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-//     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-//     'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
-//     '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
-
-// void set_keylog(uint16_t keycode, keyrecord_t *record) {
-//   char name = ' ';
-//     if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
-//         (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
-//   if (keycode < 60) {
-//     name = code_to_name[keycode];
-//   }
-
-//   // update keylog
-//   snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
-//            record->event.key.row, record->event.key.col,
-//            keycode, name);
-// }
-
-// void oled_render_keylog(void) {
-//     oled_write(keylog_str, false);
-// }
-
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_270;
 }
 
-void render_bootmagic_status(bool status) {
-    /* Show Ctrl-Gui Swap options */
-    static const char PROGMEM logo[][2][3] = {
-        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-    };
-    if (status) {
-        oled_write_ln_P(logo[0][0], false);
-        oled_write_ln_P(logo[0][1], false);
-    } else {
-        oled_write_ln_P(logo[1][0], false);
-        oled_write_ln_P(logo[1][1], false);
-    }
-}
+// void render_bootmagic_status(bool status) {
+//     /* Show Ctrl-Gui Swap options */
+//     static const char PROGMEM logo[][2][3] = {
+//         {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
+//         {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
+//     };
+//     if (status) {
+//         oled_write_ln_P(logo[0][0], false);
+//         oled_write_ln_P(logo[0][1], false);
+//     } else {
+//         oled_write_ln_P(logo[1][0], false);
+//         oled_write_ln_P(logo[1][1], false);
+//     }
+// }
 
 void print_logo_narrow(void) {
     oled_set_cursor(0,3);
